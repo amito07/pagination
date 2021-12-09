55d5b892f109c46797c98bd364caa544ef1facd0
amito07 <lianbadhon@gmail.com>
2021-12-09 23:42:05 +0600

    done

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ const axios = require('axios');
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [ggpost,setGgpost] =useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage,setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage, setPostsPerPage] = useState(5);
   useEffect(() => {
     const fetdata = async()=>{
       setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${postsPerPage}`);
+      const resdata = await axios.get('https://jsonplaceholder.typicode.com/posts')
       const posts = res.data;
+      const ggpost = resdata.data;
+      setGgpost(ggpost)
       setPosts(posts);
       setLoading(false);
     }
     fetdata()
-  }, [])
+  }, [currentPage,])
   //Current Page data
-  const indexOfLastPost = currentPage*postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
+  // const indexOfLastPost = currentPage*postsPerPage;
+  // const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  // const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
+  const currentPosts = posts
 
   //change page function
   const paginate = (pageNumber)=>setCurrentPage(pageNumber);
@@ -32,7 +37,7 @@ function App() {
     <div>
       <h1>Home Page</h1>
       <FetchData posts={currentPosts} loading={loading}/>
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
+      <Pagination postsPerPage={postsPerPage} totalPosts={ggpost.length} paginate={paginate}/>
     </div>
   );
 }
